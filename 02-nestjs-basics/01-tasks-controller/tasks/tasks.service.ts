c7af6ac6546484e948f06cfc7df165291a544cb7
Konstantin Skok <skok.konstantin@gmail.com>
2024-12-06 15:25:22 +0300

    add solution for module 1 task 1

--- 02-nestjs-basics/01-tasks-controller/tasks/tasks.service.ts
+++ 02-nestjs-basics/01-tasks-controller/tasks/tasks.service.ts
@@ -1,17 +1,50 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
 import { Task } from "./task.model";
+import { v4 as uuidv4 } from "uuid";
+import { UpdateTaskDTO } from "./dto/update-task.dto";
+import { CreateTaskDTO } from "./dto/create-task.dto";
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
 
-  getAllTasks(): Task[] {}
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
 
-  getTaskById(id: string): Task {}
+  getTaskById(id: string): Task {
+    const task = this.tasks.find((task) => task.id === id);
 
-  createTask(task: Task): Task {}
+    if (!task) {
+      throw new NotFoundException("Task not found");
+    }
+    return task;
+  }
 
-  updateTask(id: string, update: Task): Task {}
+  createTask(createTaskDTO: CreateTaskDTO): Task {
+    const task: Task = { id: uuidv4(), ...createTaskDTO };
+    this.tasks.push(task);
+    return task;
+  }
 
-  deleteTask(id: string): Task {}
+  updateTask(id: string, update: UpdateTaskDTO): Task {
+    const taskById = this.getTaskById(id);
+
+    const updatedTask: Task = { ...taskById, ...update };
+
+    this.tasks = this.tasks.map((task) => {
+      if (task.id === id) {
+        return updatedTask;
+      }
+      return task;
+    });
+
+    return updatedTask;
+  }
+
+  deleteTask(id: string): Task {
+    const taskById = this.getTaskById(id);
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+    return taskById;
+  }
 }
